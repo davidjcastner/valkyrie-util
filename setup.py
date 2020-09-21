e72612fe29650f9498b45163edd68856f50e32b7
David Castner <davidjcastner@gmail.com>
2020-09-21 11:00:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="valkyrie_util",
-    version="0.0.1",
-    author="David Castner",
-    author_email="davidjcastner@gmail.com",
-    description="A python library of various math and performance tools",
+    name='valkyrie_util',
+    version='0.0.1',
+    author='David Castner',
+    author_email='davidjcastner@gmail.com',
+    description='A python library of various math and performance tools',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/davidjcastner/valkyrie_util",
+    long_description_content_type='text/markdown',
+    url='https://github.com/davidjcastner/valkyrie_util',
     packages=setuptools.find_packages(),
     classifiers=[],
     python_requires='>=3.6',
