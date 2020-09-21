2249778a86e11950c0c5fd5cac0f737da538d0e0
David Castner <davidjcastner@gmail.com>
2020-09-21 10:55:38 -0400

    init pypi package

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="valkyrie_util",
+    version="0.0.1",
+    author="David Castner",
+    author_email="davidjcastner@gmail.com",
+    description="A python library of various math and performance tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidjcastner/valkyrie_util",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires='>=3.6',
+)
---
+++ valkyrie_util/__init__.py
@@ -0,0 +1,1 @@
+from valkyrie_util.TestClass import TestClass
