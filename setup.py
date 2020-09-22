9e7adc63fc232a46926f3b478b759b00b2074397
David Castner <davidjcastner@gmail.com>
2020-09-22 19:51:54 -0400

    Write tests for range_sum_int

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='valkyrie_util',
-    version='0.0.1',
+    version='0.1.0',
     author='David Castner',
     author_email='davidjcastner@gmail.com',
     description='A python library of various math and performance tools',
