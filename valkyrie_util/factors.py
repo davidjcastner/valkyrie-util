dfa55cab9fa92fafdbf64fe636fd1e7683502f1f
David Castner <davidjcastner@gmail.com>
2020-09-23 16:00:55 -0400

    Update factors.py

--- valkyrie_util/factors.py
+++ valkyrie_util/factors.py
@@ -22,7 +22,7 @@ def factorization_product(fact: Dict[int, int]) -> int:
     '''returns the product which yields the input factorization'''
     power: Callable[[int, int], int] = lambda b, e: b ** e
     product: Callable[[int, int], int] = lambda x, y: x * y
-    return reduce(product, map(power, fact.items()), 1)
+    return reduce(product, [power(b, e) for b, e in fact.items()], 1)
 
 
 def lcm_factorization(facts: List[Dict[int, int]]) -> Dict[int, int]:
